180301960362c3139ad663bb304d0fa1b032bce0
tinhthanh <k40cntt@gmail.com>
2021-05-17 21:58:33 +0700

    build prod

--- plugin/wefinex-server/src/environment.ts
+++ plugin/wefinex-server/src/environment.ts
@@ -1,13 +1,13 @@
 
-export const environment = {
-    production: false,
-    firebaseConfig: {
-      apiKey: "AIzaSyDNvYyw2r0JfbpGDAkhsd8T29RlFvTMJPA",
-      authDomain: "wefinex-73e57.firebaseapp.com",
-      projectId: "wefinex-73e57",
-      storageBucket: "wefinex-73e57.appspot.com",
-      messagingSenderId: "769981334831",
-      appId: "1:769981334831:web:189ddb19a9c43667afcdda",
-      measurementId: "G-RMFY10RCKG"
-    }
-  };
+  export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyApbffjUk6y7QICArn44If1pQlaoevDtLw",
+    authDomain: "wefinexbackup.firebaseapp.com",
+    projectId: "wefinexbackup",
+    storageBucket: "wefinexbackup.appspot.com",
+    messagingSenderId: "16868534353",
+    appId: "1:16868534353:web:1e33704f0ef7d599c23070",
+    measurementId: "G-VLRSMD26L3"
+  }
+};
